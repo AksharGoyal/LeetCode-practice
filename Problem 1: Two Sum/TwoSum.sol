49a0935a281bcea49de46471a688a69211db9995
Akshar Goyal <claim.akshar@gmail.com>
2024-08-13 15:41:04 +0530

    Update TwoSum.sol

--- Problem 1: Two Sum/TwoSum.sol
+++ Problem 1: Two Sum/TwoSum.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.2;
+pragma solidity 0.8.4; // Specify the Solidity version
+
+contract Solution {
+// Contracts are similar to classes as they act as a template for behaviour and can be used at various locations
+    /**
+     * @param {uint256} a
+     * @param {uint256} b
+     * @return uint256
+     */
+    function sumOfTwoIntegers(uint256 a, uint256 b)
+        public // public means the function is accessible inside and outside contract
+        pure // pure means we can not write or read storage
+        returns (uint256) // we can specify return type just like we do in C or C++
+    {
+        return a+b;
+    }
+}
+
 
